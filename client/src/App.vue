7cbdafb1655229b1564488b9a5b56eece544df9b
fedonbouzbas <fedon.bouzbas@bossinfo.ch>
2021-02-08 13:01:43 +0200

    Add AppFooter

--- client/src/App.vue
+++ client/src/App.vue
@@ -6,16 +6,17 @@
         <router-view></router-view>
       </div>
     </section>
-    <!-- <app-footer></app-footer> -->
+    <app-footer></app-footer>
   </div>
 </template>
 <script>
 import AppHeader from '@/components/AppHeader.vue'
-// import AppFooter from './AppFooter.vue'
+import AppFooter from '@/components/AppFooter.vue'
 
 export default {
   components: {
-    'app-header': AppHeader
+    'app-header': AppHeader,
+    'app-footer': AppFooter
   }
 }
 </script>
--- client/src/components/AppHeader.vue
+++ client/src/components/AppHeader.vue
@@ -1,54 +1,62 @@
 <template>
-  <div class="container">
-    <b-navbar toggleable="lg" type="dark" variant="primary" class="navbar-horizontal">
-      <!-- <b-navbar-brand href="#">PRIMARY COLOR</b-navbar-brand> -->
+  <header class="bg-dark text-center text-lg-start">
+    <div class="container">
+      <b-navbar toggleable="lg" type="dark" class="navbar-horizontal">
+        <!-- <b-navbar-brand href="#">PRIMARY COLOR</b-navbar-brand> -->
 
-      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
-      <b-collapse id="nav-collapse" is-nav>
+        <b-collapse id="nav-collapse" is-nav>
 
-        <!-- Right aligned nav items -->
-        <b-navbar-nav class="ml-left">
-          <b-nav-form>
-              <li class="nav-item">
-                <router-link class="nav-link nav-link-icon" to="/hotels">
-                  <span class="nav-link nav-link-inner--text">
-                    Hotels
-                  </span>
-                </router-link>
-              </li>
-              <!-- <li class="nav-item">
-                <router-link class="nav-link nav-link-icon" to="/books">
-                  <span class="nav-link nav-link-inner--text">
-                    Books
-                  </span>
-                </router-link>
-              </li> -->
-              <li class="nav-item">
-                <router-link class="nav-link nav-link-icon" to="/bookings">
-                  <span class="nav-link nav-link-inner--text">
-                    Bookings
-                  </span>
-                </router-link>
-              </li>
-              <li class="nav-item">
-                <router-link v-if="isAuthenticated" v-on:click.native="logout()" class="nav-link nav-link-icon" to="/">
-                  <span class="nav-link nav-link-inner--text">
-                    Logout
-                  </span>
-                </router-link>
-                <router-link v-else class="nav-link nav-link-icon" to="/login">
-                  <span class="nav-link nav-link-inner--text">
-                    Login
-                  </span>
-                </router-link>
-              </li>
-          </b-nav-form>
-        </b-navbar-nav>
-      </b-collapse>
-    </b-navbar>
-  </div>
+          <!-- Right aligned nav items -->
+          <b-navbar-nav class="ml-left">
+            <b-nav-form>
+                <li class="nav-item">
+                  <router-link class="nav-link nav-link-icon" to="/hotels">
+                    <span class="nav-link nav-link-inner--text">
+                      Hotels
+                    </span>
+                  </router-link>
+                </li>
+                <!-- <li class="nav-item">
+                  <router-link class="nav-link nav-link-icon" to="/books">
+                    <span class="nav-link nav-link-inner--text">
+                      Books
+                    </span>
+                  </router-link>
+                </li> -->
+                <li class="nav-item">
+                  <router-link class="nav-link nav-link-icon" to="/bookings">
+                    <span class="nav-link nav-link-inner--text">
+                      Bookings
+                    </span>
+                  </router-link>
+                </li>
+                <li class="nav-item">
+                  <router-link v-if="isAuthenticated" v-on:click.native="logout()" class="nav-link nav-link-icon" to="/">
+                    <span class="nav-link nav-link-inner--text">
+                      Logout
+                    </span>
+                  </router-link>
+                  <router-link v-else class="nav-link nav-link-icon" to="/login">
+                    <span class="nav-link nav-link-inner--text">
+                      Login
+                    </span>
+                  </router-link>
+                </li>
+            </b-nav-form>
+          </b-navbar-nav>
+        </b-collapse>
+      </b-navbar>
+    </div>
+  </header>
 </template>
+<style scoped>
+.navbar-default {
+    background-color: white;
+    border-color: white;
+}
+</style>
 <script>
 export default {
   computed: {
